892b3d767f33e53cf463193fa2522e82b772b91f
mitchell-devpipline <122054442+mitchell-devpipline@users.noreply.github.com>
2023-04-20 15:51:02 -0600

    finish show styles and data

--- src/components/pages/Show.js
+++ src/components/pages/Show.js
@@ -33,35 +33,51 @@ function GetShows({ params }) {
 
   return (
     <div className="show-display">
-      {show.image?.medium ? (
-        <img
-          className="show-photo"
-          src={show.image.original}
-          height={700}
-          width={500}
-          alt="show images"
-        />
-      ) : (
-        "No Image"
-      )}
-      <div className="name">
-        <h1>{show.name || "Not Found"}</h1>
+      <div className="photo2">
+        <div className="name">
+          <h1>{show.name || "Not Found"}</h1>
+          {show.image?.medium ? (
+            <img
+              className="show-photo"
+              src={show.image.original}
+              height={700}
+              width={500}
+              alt="show images"
+            />
+          ) : (
+            "No Image"
+          )}
+        </div>
       </div>
       <div className="fetch-container">
-        <div className="one">Shows Status: {show.status || "Not Found"}</div>
-        <div className="two">Language: {show.language || "Not Found"}</div>
-        <div className="three">
-          Shows Ave Rating: {show.rating?.average || "Not Found"}
-        </div>
-        <div className="four">Network: {show.network?.name || "Not Found"}</div>
-        <div className="five">
-          Shows Site: {show.officialSite || "Not Found"}
-        </div>
         <div className="six">
           <h4>Summary:</h4>
           <div
             dangerouslySetInnerHTML={{ __html: show.summary || "Not Found" }}
           />
+          <div className="show-info">
+            Language: {show.language || "Not Found"}
+          </div>
+          <div className="show-info">Genres: {show.genres || "Not Found"}</div>
+          <div className="show-info">
+            Shows Status: {show.status || "Not Found"}
+          </div>
+          <div className="show-info">
+            Premiere Date: {show.premiered || "Not Found"}
+          </div>
+          <div className="show-info">End Date: {show.ended || "Not Found"}</div>
+          <div className="show-info">
+            Shows Ave Rating: {show.rating?.average || "Not Found"}
+          </div>
+          <div className="show-info">
+            Network: {show.network?.name || "Not Found"}
+          </div>
+          <div className="show-info">
+            Network Country: {show.network?.country.code || "Not Found"}
+          </div>
+          <div className="show-info">
+            Shows Site: {show.officialSite || "Not Found"}
+          </div>
         </div>
       </div>
     </div>
